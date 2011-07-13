595be35cada2f0473d2e6ac1fb78a66bbc6a222f
Viacheslav Vasianovych <keebraa@gmail.com>
2011-07-14 00:46:04 +0300

    some test changes

--- keebraa-docs/src/main/java/com/keebraa/docs/InitialClass.java
+++ keebraa-docs/src/main/java/com/keebraa/docs/InitialClass.java
@@ -1,5 +1,11 @@
 package com.keebraa.docs;
 
+/**
+ * Initial class. Should be deleted soon
+ * 
+ * @author taqi
+ * 
+ */
 public class InitialClass
 {
 
--- keebraa-docs/src/test/java/com/keebraa/docs/InitialClassTest.java
+++ keebraa-docs/src/test/java/com/keebraa/docs/InitialClassTest.java
@@ -1,5 +1,11 @@
 package com.keebraa.docs;
 
+/**
+ * Initial test case. Should be deleted soon
+ * 
+ * @author taqi
+ * 
+ */
 public class InitialClassTest
 {
 
