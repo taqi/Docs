af35194adcc8ea1245c861b47bb0dab4ffc4ff15
Viacheslav Vasianovych <keebraa@gmail.com>
2011-07-21 09:34:11 +0300

    add: basic table model

---
+++ keebraa-docs/src/test/java/com/keebraa/docs/tables/DefaultTableModelTest.java
@@ -0,0 +1,66 @@
+package com.keebraa.docs.tables;
+
+import java.util.List;
+
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
+public class DefaultTableModelTest implements TableModel
+{
+
+    private List<Row> rows;
+    public int getRowCount()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int getColumnCount()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public String getColumnName(int columnIndex)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Class<?> getColumnClass(int columnIndex)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean isCellEditable(int rowIndex, int columnIndex)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void addTableModelListener(TableModelListener l)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void removeTableModelListener(TableModelListener l)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ keebraa-docs/src/test/java/com/keebraa/docs/tables/Row.java
@@ -0,0 +1,13 @@
+package com.keebraa.docs.tables;
+
+import javax.swing.table.TableModel;
+
+public class Row
+{
+    private TableModel table;
+    
+    public Row(TableModel table)
+    {
+        this.table = table;        
+    }
+}
