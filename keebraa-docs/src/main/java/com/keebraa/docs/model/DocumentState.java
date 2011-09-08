fb5d51beda8d651ec3989ba2abdcdaed32230c25
Viacheslav Vasianovych <keebraa@gmail.com>
2011-09-09 00:24:23 +0300

    add: javadoc

--- keebraa-docs/src/main/java/com/keebraa/docs/model/DocumentState.java
+++ keebraa-docs/src/main/java/com/keebraa/docs/model/DocumentState.java
@@ -1,8 +1,20 @@
 package com.keebraa.docs.model;
 
+import com.keebraa.docs.exceptions.DocumentHandlingException;
+
 public enum DocumentState
 {
+    /**
+     * This state should be setted to Document during instantiation. 
+     * At this case, document has been instantiated, but has not been saved yet. 
+     * From this state document cannot be moved to RECORDED state without SAVED state.
+     * This state throws {@link DocumentHandlingException} while attempting move document with this state directly to
+     * MARKED, DELETED, CANCELED states. 
+     * 
+     * Next state should be SAVED (recommended).
+     */
 	NEW,
+	
 	SAVED,
 	RECORDED,
 	CANCELED,
