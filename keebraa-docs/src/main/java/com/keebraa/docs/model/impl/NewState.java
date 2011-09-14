5e22422df0bb694122cdbcd3599b0f32bbfc4f8a
Viacheslav Vasianovych <keebraa@gmail.com>
2011-09-14 21:44:07 +0300

    intermediate commit

--- keebraa-docs/src/main/java/com/keebraa/docs/model/impl/NewState.java
+++ keebraa-docs/src/main/java/com/keebraa/docs/model/impl/NewState.java
@@ -1,19 +1,20 @@
 package com.keebraa.docs.model.impl;
 
 import com.keebraa.docs.model.DocumentState;
+
 /**
- * State for document, that has just been created. 
+ * State for document, that has just been created.
+ * 
  * @author taqi
- *
+ * 
  */
 public class NewState implements DocumentState
 {
-    
-    public static final String CAPTION="NEW";
-    
+
+    public static final String CAPTION = "NEW";
+
     public String getStateCaption()
     {
         return CAPTION;
     }
-
 }
